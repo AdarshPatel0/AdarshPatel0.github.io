1da57840a8c0128bcc27e56b9f8014c079c2fdf9
Adarsh Patel <adarsh.pate0@outlook.com>
2026-09-11 02:53:03 -0400

    Complete rewrite

--- script.js
+++
@@ -1,9 +0,0 @@
-function run(){
-    fetch("/header.html")
-    .then(response => response.text())
-    .then(html => {
-        document.getElementById("header-placeholder").innerHTML = html
-    })
-}
-
-document.addEventListener("DOMContentLoaded",run)
